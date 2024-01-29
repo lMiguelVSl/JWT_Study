b8348fcfa9c8071c90448198c81352a28dc0144e
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-01-29 17:46:20 -0500

    Add JWT config and endpoint validated

--- Api.JWT.Validation/Program.cs
+++ Api.JWT.Validation/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/protected", () => "Hello World, protected").RequireAuthorization();
 
 app.Run();
